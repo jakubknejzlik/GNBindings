8f56bac53e986cfedca7c516a9e8b90ec67dfd65
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2014-09-04 17:20:21 +0200

    Initial Commit

---
+++ GNBindings/GNBindings.h
@@ -0,0 +1,13 @@
+//
+//  GNBindings.h
+//  GNBindings
+//
+//  Created by Jakub Knejzlik on 04/09/14.
+//  Copyright (c) 2014 Jakub Knejzlik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GNBindings : NSObject
+
+@end
---
+++ GNBindings/GNBindings.m
@@ -0,0 +1,13 @@
+//
+//  GNBindings.m
+//  GNBindings
+//
+//  Created by Jakub Knejzlik on 04/09/14.
+//  Copyright (c) 2014 Jakub Knejzlik. All rights reserved.
+//
+
+#import "GNBindings.h"
+
+@implementation GNBindings
+
+@end
